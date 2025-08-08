870cb2611c94bcaf7a2745d43a2f364465303315
Ulpio <ulpionetto0@gmail.com>
2025-08-08 01:34:39 -0300

    feat: :sparkles: funcoes de filtros

--- src/main/java/com/example/persistencia/repository/UsuarioRepository.java
+++ src/main/java/com/example/persistencia/repository/UsuarioRepository.java
@@ -1,7 +1,12 @@
 package com.example.persistencia.repository;
 
 import com.example.persistencia.model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+    Page<Usuario> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
+    Page<Usuario> findByEmailContainingIgnoreCase(String email, Pageable pageable);
+    Page<Usuario> findByNomeContainingIgnoreCaseAndEmailContainingIgnoreCase(String nome,String email,Pageable pageable);
 }
